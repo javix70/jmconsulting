4ff6c3cb195da4df02d4724874160f3beb8c12c4
jai <iaver17omar@gmail.com>
2023-01-23 01:08:06 -0300

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,38 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
+    fontSize: {
+      navbar: "1.419rem",
+    },
     extend: {
       fontFamily: {
         sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
+        primary: "var(--color-granite-grey)",
+        secondary: "var(--color-almond)",
+        'granite-grey': "var(--color-granite-grey)",
+        'jelly-bean-blue': "var(--color-jelly-bean-blue)",
+        'rose-taupe': "var(--color-rose-taupe)",
+        'black-coral': "var(--color-black-coral)",
+        'nyanza': "var(--color-nyanza)",
+        'opal': "var(--color-opal)",
+        'raisin-black': "var(--color-raisin-black)",
+        'almond': "var(--color-almond)"
       },
       textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
+        default: "var(--color-almond)"
       },
       backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
+        default: "var(--color-almond)"
+      },
+      backgroundImage: {
+        'cover-wave': "url('./src/icons/cover_wave.svg')",
       },
       borderColor: {
-        default: "var(--color-border)",
+        default: "var(--color-border)"
       },
     },
   },
